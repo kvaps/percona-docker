76a98bdfa9c404bc7ac2da9e12c463da80035abd
kvaps <kvapss@gmail.com>
2019-02-25 22:16:59 +0100

    fix: ps-admin

--- percona-server.80/ps-entry.sh
+++ percona-server.80/ps-entry.sh
@@ -138,10 +138,10 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 
 		# install TokuDB engine
 		if [ -n "$INIT_TOKUDB" ]; then
-			ps-admin --docker --enable-tokudb -u root -p $MYSQL_ROOT_PASSWORD
+			ps-admin --docker --enable-tokudb -u root -p"$MYSQL_ROOT_PASSWORD"
 		fi
 		if [ -n "$INIT_ROCKSDB" ]; then
-			ps-admin --docker --enable-rocksdb -u root -p $MYSQL_ROOT_PASSWORD
+			ps-admin --docker --enable-rocksdb -u root -p"$MYSQL_ROOT_PASSWORD"
 		fi
 
 		if [ ! -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
